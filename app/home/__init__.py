69d7d812273452ea53cd56e683ee70f17991613b
Dilonne Muchingami <muchingamid@gmail.com>
2018-04-26 16:48:56 +0200

    Add method to add bay owner

--- app/home/__init__.py
+++ app/home/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from app import app
 
 home = Blueprint('home', __name__)
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
 
 from . import views
--- app/home/views.py
+++ app/home/views.py
@@ -4,136 +4,29 @@ from wtforms import ValidationError
 import requests
 import json
 
-from . import home
-from .forms import RegistrationForm
-from ..models import Patient
+from . import home, photos
+from .forms import BayOwnerForm
+from ..models import BayOwner
 from app import db
 
 
 @home.route('/')
-def index():
-    return redirect('/login')
+def dashboard():
+    render_template('home/dashboard.html', title='Dashboard')
 
 
-@home.route('/patient', methods=['GET', 'POST'])
+@home.route('/add_bay_owner', methods=['GET', 'POST'])
 @login_required
-def add_patient():
-    form = RegistrationForm()
+def add_bay_owner():
+    form = BayOwnerForm
     if form.validate_on_submit():
+        file_name = photos.save(form.photo.data)
+        file_url = photos.url(file_name)
 
-        patient = Patient(first_name=form.first_name.data, last_name=form.last_name.data,
-                          national_id=form.national_id.data)
-        patient.save()
-        return redirect('/patients')
+        bay_owner = BayOwner(first_name=form.first_name.data, last_name=form.last_name.data,
+                             national_id=form.national_id.data, upload_image_name=file_name)
+        bay_owner.save()
+        flash('Bay owner successfully created!')
+        return redirect(url_for('home.dashboard'))
 
-    else:
-        return render_template('home/patient/patient.html', form=form, title='Add Customer')
-
-
-@home.route('/patients', methods=['GET', 'POST'])
-@login_required
-def list_patients():
-    patients = Patient.get_all()
-    return render_template('home/patient/patients.html', patients=patients, title='Patients')
-
-
-@home.route('/patient/edit/<int:id>', methods=['GET', 'POST'])
-@login_required
-def edit_patient(id):
-    """
-    Edit a patient
-    """
-
-    add_patient = False
-
-    patient = Patient.query.get_or_404(id)
-    form = RegistrationForm(obj=patient)
-    if form.validate_on_submit():
-        patient.first_name = form.first_name.data
-        patient.last_name = form.last_name.data
-        patient.national_id = form.national_id.data
-        db.session.commit()
-        flash('You have successfully edited the patient.')
-
-        # redirect to the customers page
-        return redirect(url_for('home.list_patients'))
-
-    form.first_name.data = patient.first_name
-    form.last_name.data = patient.last_name
-    form.national_id.data = patient.national_id
-    return render_template('home/patient/patient.html', action="Edit",
-                           add_patient=add_patient, form=form,
-                           patient=patient, title="Edit Patient")
-
-
-@home.route('/customers/delete/<int:id>', methods=['GET', 'POST'])
-@login_required
-def delete_patient(id):
-    """
-    Delete a patient from the database
-    """
-
-    patient = Patient.query.get_or_404(id)
-    patient.delete()
-    flash('You have successfully deleted the patient.')
-
-    # redirect to the patient page
-    return redirect(url_for('home.list_patient'))
-
-
-@home.route('/patient/view/<int:id>', methods=['GET', 'POST'])
-@login_required
-def view_patient(id):
-    """
-    View a patient profile
-    """
-    patient = Patient.query.get(int(id))
-    # customer_accounts = Account.query.filter_by(customer_id=id).all()
-
-    # account = Account.query.filter_by(customer_id=id).first()
-    # account_transactions = Transaction.query.filter_by(account_number=account.mobile_number).all()
-    # print("######################### Customer Accounts", customer_accounts)
-
-    return render_template('home/patient/patient_profile.html', patient=patient, title='Patient Profile',
-                            )
-
-
-# @home.route('/patient/edit_account/<int:id>', methods=['GET', 'POST'])
-# @login_required
-# def edit_account(id):
-#     """
-#     Edit a patient account
-#     """
-#     if current_user.role_id == 1:
-#         account = Account.query.get_or_404(id)
-#         form = CustomerAccountForm(obj=account)
-#
-#         if form.validate_on_submit():
-#             account.max_value_for_transaction = form.max_value_for_transaction.data
-#             db.session.commit()
-#             flash('You have successfully edited the account.')
-#             return redirect(url_for('home.list_customers'))
-#
-#         form.max_value_for_transaction.data = account.max_value_for_transaction
-#         form.mobile_number.data = account.mobile_number
-#         # form.balance.data = account.balance
-#
-#         return render_template('home/account/edit_account.html', action="Edit",
-#                                form=form, account=account, title="Edit Account")
-#     else:
-#         abort(403)
-
-
-@home.route('/patient/add_prescription', methods=['GET', 'POST'])
-@login_required
-def create_prescription():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-
-        patient = Patient(first_name=form.first_name.data, last_name=form.last_name.data,
-                          national_id=form.national_id.data)
-        patient.save()
-        return redirect('/patients')
-
-    else:
-        return render_template('home/prescription/add.html', form=form, title='Create Prescription')
+    return render_template('home/dashboard.html', form=form, title='Dashboard')
